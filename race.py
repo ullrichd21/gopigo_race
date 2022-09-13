378aa42fc330046dbd06ea4ee35085dc02364d48
Danny Ullrich <42869122+ullrichd21@users.noreply.github.com>
2022-09-13 10:31:47 -0400

    Movement code

--- race.py
+++ race.py
@@ -3,18 +3,17 @@ import gopigo3  # import the GoPiGo3 drivers
 
 GPG = gopigo3.GoPiGo3()  # Create an instance of the GoPiGo3 class. GPG will be the GoPiGo3 object.
 
+def move_inch(inches=1):
+    rate = 9
+    for i in range(0, inches*rate):
+        GPG.set_motor_power(GPG.MOTOR_LEFT + GPG.MOTOR_RIGHT, 100)
+        time.sleep(0.01)
+        print(f"Moved {round(i/rate, 2)} inch(es)")
+def stop():
+    GPG.set_motor_power(GPG.MOTOR_LEFT + GPG.MOTOR_RIGHT, 0)
 try:
-    for i in range(0, 101):
-        GPG.set_motor_power(GPG.MOTOR_LEFT + GPG.MOTOR_RIGHT, i)
-        time.sleep(0.02)
-    # while True:
-    #     for i in range(-100, 51):
-    #         GPG.set_motor_power(GPG.MOTOR_LEFT + GPG.MOTOR_RIGHT, -i)
-    #         time.sleep(0.02)
-    #
-    #     for i in range(-50, 101):
-    #         GPG.set_motor_power(GPG.MOTOR_LEFT + GPG.MOTOR_RIGHT, i)
-    #         time.sleep(0.02)
-
+#     while True:
+    move_inch(12)
+    stop()
 except KeyboardInterrupt:  # except the program gets interrupted by Ctrl+C on the keyboard.
     GPG.reset_all()  # Unconfigure the sensors, disable the motors, and restore the LED to the control of the GoPiGo3 firmware.
